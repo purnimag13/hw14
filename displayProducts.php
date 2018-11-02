de5cd63352962d143ac95305370d06d5dff39b54
Purnima Ghosh <purnima.ghosh@pghosh.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-11-02 16:49:46 +0000

    did some things

--- displayProducts.php
+++ displayProducts.php
@@ -4,6 +4,22 @@
         <title>Display Products</title>
     </head>
     <body>
-        
+        <?php
+            require_once("db.php");
+            $sql = "select ProductName, ProductID, UnitPrice from Products group by ProductID";
+            $result = $mydb->query($sql);
+            echo 
+            "<table>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Product ID</th>
+                    <th>Unit Price</th>
+                </tr>";
+            while($row = mysqli_fetch_array($result))
+            {
+                echo "<option value='" .$row["ProductID"]. "'>" .$row["ProductID"]. "</option>"; 
+            }
+            echo "</select>";
+        ?>
     </body>
 </html>
